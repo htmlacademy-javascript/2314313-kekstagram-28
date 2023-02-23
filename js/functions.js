cc646f1fe3b589ff3a6a05af76a71dbab4388c07
Angelinaandr <angelina_korobkova_andreevna@mail.ru>
2023-02-23 11:21:01 +0300

    formatting changed

--- js/functions.js
+++ js/functions.js
@@ -1,41 +1,46 @@
 function checkSentence(sentence, symbolsAmount) {
-    if (sentence.length === symbolsAmount) {
-        return true
-    }
-    return false
+  if (sentence.length === symbolsAmount) {
+    return true;
+  }
+  return false;
 }
 
 function checkPolindrom(word) {
-    let wordForCheck = word
-        .replaceAll(' ', '')
-        .toLowerCase();
-    let reverseWord = '';
-    for (let i = 0; i < wordForCheck.length; i++) {
-        reverseWord += wordForCheck.at(wordForCheck.length - i - 1);
-    }
-    return reverseWord === wordForCheck
+  const wordForCheck = word
+    .replaceAll(' ', '')
+     .toLowerCase();
+  let reverseWord = '';
+  for (let i = 0; i < wordForCheck.length; i++) {
+     reverseWord += wordForCheck.at(wordForCheck.length - i - 1);
+  }
+  return reverseWord === wordForCheck;
 }
 
 function extractNumber(string) {
-    let number = '';
-    for (i = 0; i < string.length; i++) {
-        if (Number.isInteger(parseInt(string.at(i), 10))) {
-            number += string.at(i);
-        }
+  let number = '';
+  for (let i = 0; i < string.length; i++) {
+    if (Number.isInteger(parseInt(string.at(i), 10))) {
+      number += string.at(i);
     }
-    if (number === '') {
-        return NaN
-    }
-    return parseInt(number, 10)
+  }
+   if (number === '') {
+     return NaN;
+  }
+  return parseInt(number, 10)
 }
 
 function padStart(originalString, minLength, addSymbols) {
-    let difference = minLength - originalString.length;
-    if (originalString.length >= minLength) {
-        return originalString
-    }
-    return addSymbols.slice(0, difference % addSymbols.length) + addSymbols.repeat(difference / addSymbols.length) + originalString
+  let difference = minLength - originalString.length;
+  if (originalString.length >= minLength) {
+    return originalString;
+   }
+  return addSymbols.slice(0, difference % addSymbols.length) + addSymbols.repeat(difference / addSymbols.length) + originalString;
 }
+
+checkSentence('проверяемая строка', 20);
+checkPolindrom('топот');
+extractNumber('1 кефир, 0.5 батона');
+padStart('qwerty', 4, '0');
 
 
 
