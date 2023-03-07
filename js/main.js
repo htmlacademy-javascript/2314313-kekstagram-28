757bfe01b4e1bb9c97fc5b4f1ea5f834b586d4eb
Angelinaandr <angelina_korobkova_andreevna@mail.ru>
2023-03-07 13:54:45 +0300

    module4-task1 completed

--- js/main.js
+++ js/main.js
@@ -64,15 +64,13 @@ const createComment = () => ({
   name: NAMES[getRandomInteger(0, NAMES.length - 1)],
 });
 
-const createForm = () => {
-  return ({
+const createForm = () => ({
     id: getUniqueRandomIntegerIdUrl(),
     url: `photos/${getUniqueRandomIntegerIdUrl()}.jpg`,
     description: DESCRIPTION[getRandomInteger(0, DESCRIPTION.length - 1)],
     likes: getRandomInteger(LIKE_MIN_COUNT, LIKE_MAX_COUNT),
     comments: Array.from({ length: getRandomInteger(0, COMMENT_COUNT) }, () => createComment()),
   });
-};
 
 const createAllForm = () => Array.from({ length: PICTURE_COUNT }, () => createForm());
 createAllForm();
